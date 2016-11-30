098aca503db3360fb23f8de5ca5273f3edc4bd0d
abdv17 <sg8412@outlook.com>
2016-11-30 15:02:13 +0530

    added fadein function
    added fadein function

--- Script.js
+++ Script.js
@@ -1,2 +1,13 @@
 alert("Welcome to My Family");
-$('.main-content').fadeIn(400);
+function main(){
+// $('.skillset').hide();
+  $('#chart_div').fadeIn(1000);
+//  $('.projects').hide();
+ // $('.projects-button').on('click',function(){
+   // $(this).next().toggle();
+//    $(this).next().slideToggle(400);
+//    $(this).toggleClass('active');
+//    $(this).text('Projects Viewed');
+  });
+}
+$(document).ready(main);
